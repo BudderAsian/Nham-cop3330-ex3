aa9c97b5bd868566e131582377ac11d508010233
richa <richardnham2000@gmail.com>
2021-09-12 15:36:13 -0400

    initial commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,7 @@
 package com.example;
 import java.util.Scanner;
 /*
- *  UCF COP3330 Summer 2021 Assignment 3 Solution
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
  *  Copyright 2021 Anisha Ranjan
  */
 public class Main
